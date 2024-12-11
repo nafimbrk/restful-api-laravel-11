3276fc27254b3edf4174ba86e6becb9d139ed3ad
nafimbrk <nafimubarok2005@gmail.com>
2024-12-12 05:48:15 +0700

    done

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'image',
+        'title',
+        'content',
+    ];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
+}
